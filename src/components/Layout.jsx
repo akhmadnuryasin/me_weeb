678edb4e16d94a02fe0738308d835e7c9fb25d6c
Maudhio Andre <maudhioa@gmail.com>
2024-06-14 14:51:08 +0700

    .

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,10 +5,10 @@ import Footer from './Footer'
 const Layout = ({ children }) => {
     return (
         <div className='min-h-screen bg-black'>
-            < Header />
+            <Header />
             {children}
             <Footer />
-        </div >
+        </div>
     )
 }
 
