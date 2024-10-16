bce517421317de0864140febf0f927534ac57a1a
Nurhilman maulana <107232557+NurhilmanMaulana@users.noreply.github.com>
2024-10-16 14:20:02 +0700

    Update TopMangaSection.jsx

--- src/components/TopMangaSection.jsx
+++ src/components/TopMangaSection.jsx
@@ -95,7 +95,7 @@ const TopMangaSection = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between mb-8">
                     <h2 className="text-xl underline font-bold text-white decoration-blue-400 underline-offset-4">Top Manga</h2>
-                    <Link to={"/all/anime"} className="px-4 py-2 hover:text-white/80 text-white text-xs font-medium">View More</Link>
+                    <Link to={"/all/manga"} className="px-4 py-2 hover:text-white/80 text-white text-xs font-medium">View More</Link>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
                     {mangaData.map((manga, index) => (
